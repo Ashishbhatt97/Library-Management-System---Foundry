72d80f2c697d54eb6554ab2b7f0a1c1d4e9e19ff
Ashishbhatt97 <ashishbhatt0197@gmail.com>
2024-06-18 13:27:47 +0530

    Book issue function done

--- src/LibraryManagment.sol
+++ src/LibraryManagment.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.2;
 
 contract LibraryManagement {
-    uint256 public userCount = 0; 
+    uint256 public userCount = 0;
     address public owner;
     uint256 public booksCount = 0;
 
@@ -39,13 +39,17 @@ contract LibraryManagement {
         Gender userGender;
         string email;
         uint256 numberOfBookIssued;
-        mapping(uint256 => Book) bookIssued; 
+        mapping(uint256 => Book) bookIssued;
     }
 
     mapping(address => User) public users;
-    mapping(uint256 => Book) public books; 
+    mapping(uint256 => Book) public books;
 
     event UserAdded(uint256 userId, string name, string email);
+    event BookAdded(string title, string authorName, uint256 copiesAvailable);
+    event BookIssued(address userAddress, uint256 bookId, string title);
+    event issuedBooksByUser(uint256 userId, uint256[] bookIds);
+
 
     modifier onlyOwner() {
         require(owner == msg.sender, "You cannot make these changes!");
@@ -55,19 +59,18 @@ contract LibraryManagement {
     function addUser(string memory _userName, string memory _email) public {
         userCount++;
 
-        User storage newUser = users[msg.sender]; 
+        User storage newUser = users[msg.sender];
         newUser.userName = _userName;
         newUser.userId = userCount;
         newUser.userRole = Role.Member;
-        newUser.userGender = Gender.Male; 
+        newUser.userGender = Gender.Male;
         newUser.email = _email;
         newUser.numberOfBookIssued = 0;
 
         emit UserAdded(userCount, _userName, _email);
     }
 
-  event BookAdded(string title, string authorName, uint256 copiesAvailable);
-   
+
     function addBook(
         string memory _title,
         string memory _authorName,
@@ -91,7 +94,35 @@ contract LibraryManagement {
         emit BookAdded(_title, _authorName, _copiesAvailable);
     }
 
+  
+    function bookIssue(uint256 _bookId, address userAddress) public onlyOwner {
+        require(books[_bookId].copiesAvailable > 0, "No more copies available");
 
- 
+        User storage user = users[userAddress];
+        require(
+            user.numberOfBookIssued < 5,
+            "You cannot take more than 5 books"
+        );
 
+        uint256 booksIssuedIndex = user.numberOfBookIssued;
+        user.bookIssued[booksIssuedIndex] = books[_bookId];
+
+        user.numberOfBookIssued++;
+        books[_bookId].copiesAvailable--;
+
+        emit BookIssued(userAddress, _bookId, books[_bookId].title);
+    }
+
+    function booksIssuedByUser(
+        address _userAddress
+    ) public onlyOwner returns (uint256[] memory) {
+        User storage user = users[_userAddress];
+        uint256[] memory issuedBooks = new uint256[](user.numberOfBookIssued);
+
+        for (uint256 i = 0; i < user.numberOfBookIssued; i++) {
+            issuedBooks[i] = user.bookIssued[i].bookId;
+        }
+        emit issuedBooksByUser(user.userId, issuedBooks);
+        return issuedBooks;
+    }
 }
